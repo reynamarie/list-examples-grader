2689351ff1695bc457fb61e2700ff293d8db5314
Reyna Maharaj <146615719+reynamarie@users.noreply.github.com>
2024-05-28 14:19:50 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,29 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    echo "ListExamples.java file found."
+else
+    echo "ListExamples.java file not found." 
+    echo "Grade: 0"
+    exit
+fi
+
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+echo "The exit code for the copilation is $?" 
+if[[$? -ne 0]]
+then
+    echo "Grade: 0"
+    exit
+fi
+
+echo "hello $?"
+
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
